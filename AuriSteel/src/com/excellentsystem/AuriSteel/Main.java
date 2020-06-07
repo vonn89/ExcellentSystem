eb6575e17b3be58250cc9e109eb6977bc73638c0
Excellent <Excellent@DESKTOP-S7B560I>
2020-06-07 22:33:06 +0700

    splash screen

--- AuriSteel/src/com/excellentsystem/AuriSteel/Main.java
+++ AuriSteel/src/com/excellentsystem/AuriSteel/Main.java
@@ -35,6 +35,7 @@ import com.excellentsystem.AuriSteel.View.Dialog.KategoriKeuanganController;
 import com.excellentsystem.AuriSteel.View.Dialog.KategoriPiutangController;
 import com.excellentsystem.AuriSteel.View.Dialog.KategoriTransaksiController;
 import com.excellentsystem.AuriSteel.View.Dialog.MessageController;
+import com.excellentsystem.AuriSteel.View.Dialog.SplashScreenController;
 import com.excellentsystem.AuriSteel.View.Dialog.UbahPasswordController;
 import com.excellentsystem.AuriSteel.View.HutangController;
 import com.excellentsystem.AuriSteel.View.KeuanganController;
@@ -81,23 +82,24 @@ import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import javafx.animation.Animation;
 import javafx.animation.Transition;
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -134,65 +136,102 @@ public class Main extends Application {
     public static SecretKeySpec key;
     @Override
     public void start(Stage stage)  {
-        try{  
+        MainStage = stage;
+        MainStage.setTitle("Auri Steel Metalindo");
+        MainStage.setMaximized(true);
+        MainStage.getIcons().add(new Image(getClass().getResourceAsStream("Resource/icon.png")));
+        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 //            Font.loadFont(getClass().getResourceAsStream("Resource/Lato-Regular.ttf"), 12);
 //            Font.loadFont(getClass().getResourceAsStream("Resource/Lato-Bold.ttf"), 12);
 //            Font.loadFont(getClass().getResourceAsStream("Resource/BodonBk.ttf"), 12);
-            String password = "password";
-            byte[] salt = "12345678".getBytes();
-            key = createSecretKey(password.toCharArray(), salt, 40000, 128);
-            
-            try(Connection con = Koneksi.getConnection()){
-                MainStage = stage;
-                MainStage.setTitle("Auri Steel Metalindo");
-                MainStage.setMaximized(true);
-                MainStage.getIcons().add(new Image(getClass().getResourceAsStream("Resource/icon.png")));
-                screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-                
-                System.out.println(new Date()+" start");
-                sistem = SistemDAO.getSystem(con);
-                sistem.setListKategoriKeuangan(KategoriKeuanganDAO.getAll(con));
-                sistem.setListKategoriTransaksi(KategoriTransaksiDAO.getAllByStatus(con, "true"));
-                sistem.setListGudang(GudangDAO.getAll(con));
-                sistem.setListKategoriBahan(KategoriBahanDAO.getAllByStatus(con, "true"));
-                sistem.setListKategoriHutang(KategoriHutangDAO.getAll(con));
-                sistem.setListKategoriPiutang(KategoriPiutangDAO.getAll(con));
-                List<User> listUser = UserDAO.getAll(con);
-                List<Otoritas> listOtoritas = OtoritasDAO.getAll(con);
-                for(User u : listUser){
-                    List<Otoritas> otoritas = new ArrayList<>();
-                    for(Otoritas o : listOtoritas){
-                        if(u.getKodeUser().equals(o.getKodeUser()))
-                            otoritas.add(o);
+        ProgressBar progress = new ProgressBar();
+        Label updateLabel = new Label();
+        Task<String> task = new Task<String>() {
+            @Override 
+            public String call() throws Exception{
+                updateMessage("connecting to server...");
+                updateProgress(10, 100);
+                try (Connection con = Koneksi.getConnection()) {
+                    updateProgress(20, 100);
+                    String password = "password";
+                    byte[] salt = "12345678".getBytes();
+                    key = createSecretKey(password.toCharArray(), salt, 40000, 128);
+                    updateProgress(30, 100);
+                    updateMessage("initializing system...");
+                    sistem = SistemDAO.getSystem(con);
+                    sistem.setListKategoriKeuangan(KategoriKeuanganDAO.getAll(con));
+                    sistem.setListKategoriTransaksi(KategoriTransaksiDAO.getAllByStatus(con, "true"));
+                    sistem.setListGudang(GudangDAO.getAll(con));
+                    sistem.setListKategoriBahan(KategoriBahanDAO.getAllByStatus(con, "true"));
+                    sistem.setListKategoriHutang(KategoriHutangDAO.getAll(con));
+                    sistem.setListKategoriPiutang(KategoriPiutangDAO.getAll(con));
+                    List<User> listUser = UserDAO.getAll(con);
+                    List<Otoritas> listOtoritas = OtoritasDAO.getAll(con);
+                    for(User u : listUser){
+                        List<Otoritas> otoritas = new ArrayList<>();
+                        for(Otoritas o : listOtoritas){
+                            if(u.getKodeUser().equals(o.getKodeUser()))
+                                otoritas.add(o);
+                        }
+                        u.setOtoritas(otoritas);
                     }
-                    u.setOtoritas(otoritas);
-                }
-                sistem.setListUser(listUser);
-                if(!version.equals(sistem.getVersion())){
-                    String status = Function.downloadUpdateGoogleStorage("Auri Steel.exe");
-                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                    alert.setTitle("Information");
-                    alert.setContentText(status);
-                    alert.showAndWait();
-                    System.exit(0);
-                    System.out.println(new Date()+" download update");
-                }
+                    sistem.setListUser(listUser);
+                    updateProgress(40, 100);
+                    updateMessage("checking for updates...");
+                    if(!version.equals(sistem.getVersion())){
+                        updateMessage("updating software...");
+                        updateProgress(50, 100);
+                        String status = Function.downloadUpdateGoogleStorage("Auri Steel.exe");
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Information");
+                        alert.setContentText(status);
+                        alert.showAndWait();
+                        System.exit(0);
+                    }
 //                Service.createAbsensi(con, 01, 2020);
 //                System.out.println(Function.getServerDate(con).getDate());
 ////                if(Function.getServerDate(con).getDate()==1){
 //                    Service.setPenyusutanAset(con);
 ////                }
 //                System.out.println(new Date()+" penyusutan aset");
-                showLoginScene();
+                    updateProgress(70, 100);
+                    Thread.sleep(500);
+                    updateProgress(80, 100);
+                    Thread.sleep(500);
+                    updateProgress(90, 100);
+                    Thread.sleep(500);
+                    updateProgress(100, 100);
+                    return "true";
+                }
             }
-        }catch(Exception e){
-            e.printStackTrace();
+        };
+        task.setOnRunning((e) -> {
+            showSplashScreen(progress, updateLabel);
+        });
+        task.setOnSucceeded((e) -> {
+            splash.close();
+            if(task.getValue().equals("true")){
+                showLoginScene();
+            }else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setContentText("Application Failed - \n" +task.getValue());
+                alert.showAndWait();
+                System.exit(0);
+            }
+        });
+        task.setOnFailed((e) -> {
+            task.getException().printStackTrace();
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error");
-            alert.setContentText("Application error - \n" +e);
+            alert.setContentText("Application error - \n" +task.getException());
             alert.showAndWait();
             System.exit(0);
-        }
+            splash.close();
+        });
+        progress.progressProperty().bind(task.progressProperty());
+        updateLabel.textProperty().bind(task.messageProperty());
+        new Thread(task).start();
     }
     public void showLoginScene() {
         try{
@@ -201,27 +240,9 @@ public class Main extends Application {
             AnchorPane container = (AnchorPane) loader.load();
             
             Scene scene = new Scene(container);
-            final Animation animationshow = new Transition() {
-                { setCycleDuration(Duration.millis(1000)); }
-                @Override
-                protected void interpolate(double frac) {
-                    MainStage.setOpacity(1-frac);
-                }
-            };
-            animationshow.onFinishedProperty().set((EventHandler<ActionEvent>) (ActionEvent actionEvent) -> {
-                final Animation animation = new Transition() {
-                    { setCycleDuration(Duration.millis(1000)); }
-                    @Override
-                    protected void interpolate(double frac) {
-                        MainStage.setOpacity(frac);
-                    }
-                };
-                animation.play();
-                MainStage.hide();
-                MainStage.setScene(scene);
-                MainStage.show();
-            });
-            animationshow.play();
+            MainStage.hide();
+            MainStage.setScene(scene);
+            MainStage.show();
             LoginController controller = loader.getController();
             controller.setMainApp(this);
         }catch(Exception e){
@@ -694,6 +715,46 @@ public class Main extends Application {
             e.printStackTrace();
             showMessage(Modality.NONE, "Error", e.toString());
             return null;
+        }
+    }
+    private Stage splash;
+    public void showSplashScreen(ProgressBar progressBar, Label updateLabel){
+        try{
+            if(splash!=null)
+                splash.close();
+            splash = new Stage();
+            splash.initModality(Modality.WINDOW_MODAL);
+            splash.initOwner(MainStage);
+            splash.initStyle(StageStyle.TRANSPARENT);
+            splash.setOnCloseRequest((event) -> {
+                event.consume();
+            });
+            
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("View/Dialog/SplashScreen.fxml"));
+            AnchorPane container = (AnchorPane) loader.load();
+            SplashScreenController controller = loader.getController();
+            progressBar.setPrefWidth(475);
+            updateLabel.setStyle("-fx-text-fill: white");
+            controller.setSplashScreen(progressBar, updateLabel);
+
+            Scene scene = new Scene(container);
+            scene.setFill(Color.TRANSPARENT);
+            
+            splash.hide();
+            splash.setScene(scene);
+            splash.show();
+            
+            splash.setHeight(screenSize.getHeight());
+            splash.setWidth(screenSize.getWidth());
+            splash.setX((screenSize.getWidth() - splash.getWidth()) / 2);
+            splash.setY((screenSize.getHeight() - splash.getHeight()) / 2);
+        }catch(Exception e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(MainStage);
+            alert.setTitle("Error");
+            alert.setContentText("Application error - \n" +e);
+            alert.showAndWait();
         }
     }
     public void closeDialog(Stage owner,Stage dialog){
---
+++ AuriSteel/src/com/excellentsystem/AuriSteel/View/Dialog/SplashScreenController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.excellentsystem.AuriSteel.View.Dialog;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Excellent
+ */
+public class SplashScreenController {
+
+    @FXML VBox vbox;
+    public void initialize() {
+    }    
+    public void setSplashScreen(ProgressBar p, Label l){
+        vbox.getChildren().add(l);
+        vbox.getChildren().add(p);
+    }
+}
